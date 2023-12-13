cc1576b2b8895ac0651cb48eb6f3bfebdc7da99f
ShineZawIT <shinezaw9531@gmail.com>
2023-12-14 02:59:03 +0630

    -- Add React and Asp.net Web Api project

---
+++ AspWebApi/AspWebApi/Models/EmployeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReactAspCrudApi.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set;}
+    }
+}
